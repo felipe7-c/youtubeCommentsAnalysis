adeebbcfa45399b77330d4a4edda494d367a7cc8
felipe_cruz <felipe.sousa@biomtech.com.br>
2026-03-09 18:46:39 -0300

    Cleaning comments

---
+++ src/data/cleaning.py
@@ -0,0 +1,40 @@
+import re
+
+class DataProcessing:
+    def __init__(self, comments: list[str]):
+        self.comments = comments
+
+    def cleaningData(self):
+
+        cleaned_comments = []
+
+        for comment in self.comments:
+            cleaned_comment = self.transformingData(comment)
+            cleaned_comments.append(cleaned_comment)
+        
+        return cleaned_comments
+    
+    def transformingData(self, comment: str):
+
+        comment = comment.lower()
+
+        # remove links
+        comment = re.sub(r"http\S+|www\S+", "", comment)
+
+        # remove menções
+        comment = re.sub(r"@\w+", "", comment)
+
+        # remove hashtags
+        comment = re.sub(r"#\w+", "", comment)
+
+        # remove emojis
+        comment = re.sub(r"[^\w\sà-úÀ-Ú]", "", comment)
+
+        # remove números
+        comment = re.sub(r"\d+", "", comment)
+
+        # remove espaços duplicados
+        comment = re.sub(r"\s+", " ", comment)
+
+        return comment.strip()
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from src.usecases.collectCommentsUsecase import CollectCommentsUsecase
+from src.data.cleaning import DataProcessing
 
 load_dotenv()
 api_key = os.getenv("api_key")
@@ -14,6 +15,14 @@ def main():
 
     #Collect Videos Comments
     comments = inst_collectData.collectCommentsVideos()
+
+    inst_comments_treat = DataProcessing(comments)
+    
+    treated_data = inst_comments_treat.cleaningData()
+
+
+
+        
 
     return
 
