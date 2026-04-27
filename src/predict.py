6b1f2eb7059a5e56d8e95286047bb9d473405911
felipe_cruz <felipe.sousa@biomtech.com.br>
2026-04-27 17:32:03 -0300

    Atualização de predict.py removendo nltk e tokenize devido ao novo modelo

--- src/predict.py
+++ src/predict.py
@@ -1,63 +1,49 @@
-import pickle
-import nltk
-from nltk.tokenize import word_tokenize
 from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
+from sentence_transformers import SentenceTransformer
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('rslp')
 
 class CommentRequest(BaseModel):
     text: str
 
 class PredictionService:
     def __init__(self):
-        self.vectorizer, self.label_map, self.model = self.load_artifacts()
-        self.stop_words = nltk.corpus.stopwords.words('portuguese')
-        self.stemmer = nltk.stem.RSLPStemmer()
+        self.embedding_model, self.label_map, self.model = self.load_artifacts()
 
     def load_artifacts(self):
-        with open("models/vectorizer.pkl", "rb") as f:
-            vectorizer = pickle.load(f)
+        # embedding model 
+        embedding_model = SentenceTransformer("models/embedding_model")
 
+        # label map
         with open("models/label_map.pkl", "rb") as f:
             label_map = pickle.load(f)
 
+        # logistic regression model
         with open("models/model.pkl", "rb") as f:
             model = pickle.load(f)
 
-        return vectorizer, label_map, model
-    
-    def preprocess(self, text):
-        text = text.lower()
-        tokens = word_tokenize(text, language='portuguese')
+        return embedding_model, label_map, model
 
-        tokens = [w for w in tokens if w not in self.stop_words]
-        tokens = [self.stemmer.stem(w) for w in tokens]
+    def predict(self, text: str):
 
-        return ' '.join(tokens)
-
-
-    def predict(self, text):
-        processed = self.preprocess(text)
-
-        X = self.vectorizer.transform([processed])
+        X = self.embedding_model.encode([text])
 
         pred = self.model.predict(X)[0]
 
+        # inversão do label map
         inv_map = {v: k for k, v in self.label_map.items()}
 
         return inv_map[pred]
 
-app = FastAPI()
 
+app = FastAPI()
 service = PredictionService()
+
 
 @app.post("/predict")
 def predict_endpoint(request: CommentRequest):
-    try: 
+    try:
         return {"prediction": service.predict(request.text)}
     except Exception as e:
         return {"error": str(e)}
